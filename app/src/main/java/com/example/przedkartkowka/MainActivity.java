8925bd525e48a9f241d40e878321b656ebd4feab
barte28 <bartek.teuerle@gmail.com>
2025-03-26 11:20:36 +0100

    zwykle losowanie

--- app/src/main/java/com/example/przedkartkowka/MainActivity.java
+++ app/src/main/java/com/example/przedkartkowka/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.przedkartkowka;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textViewLiczba;
+    private Button button;
+    private int liczba;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        textViewLiczba = findViewById(R.id.textViewLiczba);
+
+        button.setOnClickListener(
+            new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random random = new Random();
+                liczba = random.nextInt(100)+1;
+                textViewLiczba.setText(String.valueOf(liczba));
+            }
+        });
+
     }
 }
