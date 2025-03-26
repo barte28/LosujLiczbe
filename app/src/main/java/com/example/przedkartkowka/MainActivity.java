4c04cd151815a9555d4232117f41f65cc373cdeb
barte28 <bartek.teuerle@gmail.com>
2025-03-26 11:33:56 +0100

    binding i lepsze losowanie

--- app/src/main/java/com/example/przedkartkowka/MainActivity.java
+++ app/src/main/java/com/example/przedkartkowka/MainActivity.java
@@ -7,28 +7,29 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.przedkartkowka.databinding.ActivityMainBinding;
+
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView textViewLiczba;
-    private Button button;
-    private int liczba;
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //ActivityMain Binding + 3 następne linijki binding + build Features w build gradle
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        Random random = new Random();
 
-        button = findViewById(R.id.button);
-        textViewLiczba = findViewById(R.id.textViewLiczba);
 
-        button.setOnClickListener(
+        binding.button.setOnClickListener(
             new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Random random = new Random();
-                liczba = random.nextInt(100)+1;
-                textViewLiczba.setText(String.valueOf(liczba));
+                int liczba = random.nextInt(100);
+                binding.textViewLiczba.setText(String.valueOf(liczba));
             }
         });
 
